f9d060d2cc7e1c1d2c3995a98ccadd57b4d25124
Chris Eve Parravicini <chris.parravicini@gmail.com>
2022-03-27 21:22:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,235 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+
+const myQuestions = [
+    {
+      question: "Choose your favorite season",
+      answers: {
+        a: "Winter",
+        b: "Summer",
+        c: "Spring",
+        d: "Fall"
+      },
+      answerA: "a",
+      answerB: "b",
+      answerC: "c",
+      answerD: "d"
+    },
+    {
+      question: "Pick a color:",
+      answers: {
+        a: "Yellow",
+        b: "Blue",
+        c: "Red"
+      },
+      answerA: "a",
+      answerB: "b",
+      answerC: "c",
+      answerD: "d"
+    },
+    {
+        question: "Choose an item:",
+        answers: {
+          a: "A Painting",
+          b: "A Guitar",
+          c: "Candy"
+        },
+        answerA: "a",
+        answerB: "b",
+        answerC: "c",
+        answerD: "d"
+      },
+      {
+        question: "Choose a drink:",
+        answers: {
+          a: "Coffee",
+          b: "Margarita",
+          c: "Red Wine"
+        },
+        answerA: "a",
+        answerB: "b",
+        answerC: "c",
+        answerD: "d"
+      },
+      {
+        question: "Pick a word, any word:",
+        answers: {
+          a: "Magnolia",
+          b: "Ivy",
+          c: "Leilani"
+        },
+        answerA: "a",
+        answerB: "b",
+        answerC: "c",
+        answerD: "d"
+      },
+      
+  ];
+
+function buildQuiz(){
+
+     // variable to store the HTML output
+  const output = [];
+
+  // for each question...
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+
+      // variable to store the list of possible answers
+      const answers = [];
+
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+    //   add this question and its answers to the output
+      output.push(
+        `<div class = "slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>
+        </div>`
+
+      );
+    }
+  );
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+}
+
+function showResults(){
+
+     // gather answer containers from our quiz
+  const answerContainers = quizContainer.querySelectorAll('.answers');
+  // keep track of user's answers
+//   let numCorrect = 0;
+
+  // for each question...
+  myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if(userAnswer == currentQuestion.answerA){
+      // add to the number of correct answers
+    //   numCorrect++;
+    // resultsContainer.innerHTML = `Congrats! You are Willow`
+
+    submitButton.addEventListener('click', () => {
+
+        Swal.fire({
+            title: 'Congrats!',
+            text: 'You are Willow',
+            imageUrl: 'https://64.media.tumblr.com/97e12e6afa755ee09ca87b89526dd9a5/a634737d40c74cdd-fa/s1280x1920/960114ef1b281ba52abd3190efe1c7cc9d66e31a.jpg',
+            // imageWidth: 400,
+            imageHeight: 300,
+            imageAlt: 'Willow',
+          })
+    
+        });
+
+      // color the answers green
+    //   answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else if (userAnswer == currentQuestion.answerB) {
+      // color the answers red
+    //   answerContainers[questionNumber].style.color = 'red';
+    // resultsContainer.innerHTML = `Congrats! You are Juniper`
+
+
+    submitButton.addEventListener('click', () => {
+
+        Swal.fire({
+            title: 'Congrats!',
+            text: 'You are Juniper',
+            imageUrl: 'https://64.media.tumblr.com/15c5027619150ba2e645f6e019813134/tumblr_pk4hn4cgfB1slaih9_1280.jpg',
+            // imageWidth: 400,
+            imageHeight: 300,
+            imageAlt: 'Juniper',
+          })
+    
+        });
+
+    }else 
+    // resultsContainer.innerHTML = `Congrats! You are Hazel`
+
+    submitButton.addEventListener('click', () => {
+
+        Swal.fire({
+            title: 'Congrats!',
+            text: 'You are Hazel',
+            imageUrl: 'https://glittermagazine.co/wp-content/uploads/2020/10/cfb2b2457305f5e6afe3023a9cc3879c.png',
+            // imageWidth: 400,
+            imageHeight: 300,
+            imageAlt: 'Hazel',
+          })
+    
+        });
+
+  });
+  // show number of correct answers out of total
+//   resultsContainer.innerHTML = `Congrats! You are Willow`
+//   resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+  
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+
+// display quiz right away
+buildQuiz();
+
+// Pagination
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+showSlide(currentSlide);
+
+// on submit, show results
+submitButton.addEventListener('mouseover', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
